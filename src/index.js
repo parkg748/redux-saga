bd83ff664c6853fa51be9d12816e4cc7bb2e701d
Grace Park <parkg748@newschool.edu>
2019-02-03 06:47:19 -0800

    project setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://rem-rest-api.herokuapp.com/api';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
