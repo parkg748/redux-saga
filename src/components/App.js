99b8ed472a4d2c29ff5635a88fc1e708220740a2
Grace Park <parkg748@newschool.edu>
2019-02-03 07:16:58 -0800

    javascript generator functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 
+function* testing() {
+  while(true) {
+    yield 1;
+    yield 2;
+    yield 3;
+  }
+}
+
 class App extends Component {
   render() {
+    const iterator = testing();
+    console.log(iterator.next());
+    console.log(iterator.next());
+    console.log(iterator.next());
+    console.log(iterator.next());
+
     return (
       <div>test</div>
     );
