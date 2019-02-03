b2171f109e6a8b7af676aae99ff7924bf1903201
Grace Park <parkg748@newschool.edu>
2019-02-03 07:52:56 -0800

    users list ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getUsersRequest } from '../actions/users';
+import UsersList from './UsersList';
 
 class App extends Component {
   constructor(props) {
@@ -10,12 +11,16 @@ class App extends Component {
   }
 
   render() {
+    const users = this.props.users;
+
     return (
-      <div>test</div>
+      <div style={{ margin: '0 auto', padding: '20px', maxWidth: '600px' }}>
+        <UsersList users={users.items} />
+      </div>
     );
   }
 }
 
-export default connect(null, {
+export default connect(({ users }) => ({ users }), {
   getUsersRequest
 })(App);
