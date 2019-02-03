e2a408dcdf483a17b90a804f43eba210c52bdf80
Grace Park <parkg748@newschool.edu>
2019-02-03 07:28:56 -0800

    finish get users saga

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,7 @@
 import React, { Component } from 'react';
 
-function* testing() {
-  while(true) {
-    yield 1;
-    yield 2;
-    yield 3;
-  }
-}
-
 class App extends Component {
   render() {
-    const iterator = testing();
-    console.log(iterator.next());
-    console.log(iterator.next());
-    console.log(iterator.next());
-    console.log(iterator.next());
-
     return (
       <div>test</div>
     );
