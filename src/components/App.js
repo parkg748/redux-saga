4a4a7baff32adb29b98ca50415448e002de6a7d6
Grace Park <parkg748@newschool.edu>
2019-02-03 09:14:16 -0800

    displaying errors in ui ;

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { getUsersRequest, createUserRequest, deleteUserRequest } from '../actions/users';
+import { getUsersRequest, createUserRequest, deleteUserRequest, usersError } from '../actions/users';
 import UsersList from './UsersList';
 import NewUserForm from './NewUserForm';
+import { Alert } from 'reactstrap';
 
 class App extends Component {
   constructor(props) {
@@ -22,11 +23,20 @@ class App extends Component {
     this.props.deleteUserRequest(userId);
   };
 
+  handleCloseAlert = () => {
+    this.props.usersError({
+      error: ''
+    });
+  };
+
   render() {
     const users = this.props.users;
 
     return (
       <div style={{ margin: '0 auto', padding: '20px', maxWidth: '600px' }}>
+        <Alert color="danger" isOpen={!!this.props.users.error} toggle={this.handleCloseAlert}>
+          {this.props.users.error}
+        </Alert>
         <NewUserForm onSubmit={this.handleSubmit}/>
         <UsersList users={users.items} onDeleteUser={this.handleDeleteUserClick}/>
       </div>
@@ -37,5 +47,6 @@ class App extends Component {
 export default connect(({ users }) => ({ users }), {
   getUsersRequest,
   createUserRequest,
-  deleteUserRequest
+  deleteUserRequest,
+  usersError
 })(App);
